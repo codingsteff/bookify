722b7a7717531d09f6278e533144c7d15a9c2c0d
Stefan Bättig <stefan.baettig@viag.ch>
2024-12-31 13:14:11 +0000

    Add SqlConnection Factory

--- src/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,15 @@
 using Bookify.Application.Abstractions.Clock;
+using Bookify.Application.Abstractions.Data;
 using Bookify.Application.Abstractions.Email;
 using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings;
 using Bookify.Domain.Users;
 using Bookify.Infrastructure.Clock;
+using Bookify.Infrastructure.Data;
 using Bookify.Infrastructure.Email;
 using Bookify.Infrastructure.Repositories;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,6 +40,9 @@ public static class DependencyInjection
         services.AddScoped<IBookingRepository, BookingRepository>();
         // use service provider to resolve ApplicationDbContext as the implementation of IUnitOfWork
         services.AddScoped<IUnitOfWork>(sp => sp.GetRequiredService<ApplicationDbContext>());
+
+        services.AddSingleton<ISqlConnectionFactory>(_ => new SqlConnectionFactory(connectionString));
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler()); // Register custom Dapper type handler for DateOnly conversion
 
         return services;
     }
