e3298ef33986870b6b1e871492eeab434c0d9be8
Stefan Bättig <stefan.baettig@viag.ch>
2024-12-31 12:40:57 +0000

    Add EfCore

--- src/Bookify.Domain/Abstractions/IUnitOfWork.cs
+++ src/Bookify.Domain/Abstractions/IUnitOfWork.cs
@@ -1,4 +1,6 @@
 namespace Bookify.Domain.Abstractions;
+
+// Implementation in ApplicationDbContext: SaveChangesAsync same signature
 public interface IUnitOfWork
 {
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
