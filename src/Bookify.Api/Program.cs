5071c1de4928162befc30060f8439bcbb2f9e009
Stefan Bättig <stefan.baettig@viag.ch>
2025-01-01 12:24:53 +0000

    Add Reference to Application+Infrastrucutre

--- src/Bookify.Api/Program.cs
+++ src/Bookify.Api/Program.cs
@@ -1,7 +1,13 @@
+using Bookify.Application;
+using Bookify.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
