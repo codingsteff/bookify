be37c155a0b8107efecafb02806a67c91190a0cd
Stefan Bättig <stefan.baettig@viag.ch>
2025-01-01 15:16:19 +0000

    Add Controllers and Endoints

---
+++ src/Bookify.Api/Controllers/Bookings/ReserveBookingRequest.cs
@@ -0,0 +1,9 @@
+namespace Bookify.Api.Controllers.Bookings;
+
+// Instead of using directly the ReserveBookingCommand, we can use a record to represent the request
+// We don't want to expose our internal Command (loosely coupled)
+public sealed record ReserveBookingRequest(
+    Guid ApartmentId,
+    Guid UserId,
+    DateOnly StartDate,
+    DateOnly EndDate);
