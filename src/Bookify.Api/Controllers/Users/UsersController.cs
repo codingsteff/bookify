63a4ff82320ddc805408601ba5092aa7088e509d
Stefan Bättig <stefan.baettig@viag.ch>
2025-01-05 14:26:55 +0000

    Add User LogIn

--- src/Bookify.Api/Controllers/Users/UsersController.cs
+++ src/Bookify.Api/Controllers/Users/UsersController.cs
@@ -1,5 +1,5 @@
 using Bookify.Api.Controllers.Users;
-using Bookify.Application.Apartments.SearchApartments;
+using Bookify.Application.Users.LogInUser;
 using Bookify.Application.Users.RegisterUser;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -20,16 +20,30 @@ public class UsersController : ControllerBase
 
     [AllowAnonymous]
     [HttpPost("register")]
-    public async Task<IActionResult> RegisterUser(
-            RegisterUserRequest request,
-            CancellationToken cancellationToken)
+    public async Task<IActionResult> RegisterUser(RegisterUserRequest request, CancellationToken cancellationToken)
     {
-
         var command = new RegisterUserCommand(
             request.Email,
             request.FirstName,
             request.LastName,
             request.Password);
+
+        var result = await _sender.Send(command, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok(result.Value);
+    }
+
+    [AllowAnonymous]
+    [HttpPost("login")]
+    // login user: api act as a proxy to keycloak
+    public async Task<IActionResult> LogInUser(LogInUserRequest request, CancellationToken cancellationToken)
+    {
+        var command = new LogInUserCommand(request.Email, request.Password);
 
         var result = await _sender.Send(command, cancellationToken);
 
