53c759bbb9adcae3b33f481f2b5c49f863d17536
Stefan Bättig <stefan.baettig@viag.ch>
2025-07-14 08:42:44 +0000

    feat(tests): add user registration functional tests

--- test/Bookify.Api.FunctionalTests/Users/UserData.cs
+++ test/Bookify.Api.FunctionalTests/Users/UserData.cs
@@ -4,5 +4,5 @@ namespace Bookify.Api.FunctionalTests.Users;
 
 internal static class UserData
 {
-    public static RegisterUserRequest RegisterTestUserRequest => new("test@test.com", "Hans", "Schwarzenegger", "123456");
+    public static RegisterUserRequest RegisterTestUserRequest => new("functional-test@user.com", "first", "last", "123456");
 }
