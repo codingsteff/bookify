7b49b92d5de4aaa26f6aa1ea13332c27572a1048
Stefan Bättig <stefan.baettig@viag.ch>
2025-07-14 07:13:21 +0000

    Add functional tests project

---
+++ test/Bookify.Api.FunctionalTests/Infrastructure/BaseIntegrationTest.cs
@@ -0,0 +1,28 @@
+using Bookify.Api.Controllers.Users;
+using Bookify.Api.FunctionalTests.Users;
+using Bookify.Application.Users.LogInUser;
+
+namespace Bookify.Api.FunctionalTests.Infrastructure;
+
+public abstract class BaseIntegrationTest : IClassFixture<FunctionalTestWebAppFactory>
+{
+    protected readonly HttpClient HttpClient;
+
+    protected BaseIntegrationTest(FunctionalTestWebAppFactory factory)
+    {
+        HttpClient = factory.CreateClient();
+    }
+
+    protected async Task<string> GetAccessToken()
+    {
+        var loginResponse = await HttpClient.PostAsJsonAsync(
+            "/api/v1/users/login",
+            new LogInUserRequest(
+                UserData.RegisterTestUserRequest.Email,
+                UserData.RegisterTestUserRequest.Password));
+
+        var accessTokenResponse = await loginResponse.Content.ReadFromJsonAsync<AccessTokenResponse>();
+        return accessTokenResponse!.AccessToken;
+    }
+
+}
